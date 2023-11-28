695177ad42b1e82cf59f918c5bf6829ce33d4196
AgustinGonzalez <agustin_.gonzalez_@hotmail.com>
2023-11-28 16:41:18 -0300

    colors

--- front/rastrea-patitas/src/App.jsx
+++ front/rastrea-patitas/src/App.jsx
@@ -2,19 +2,18 @@ import { Button } from "@nextui-org/react";
 import React, { useEffect, useState } from "react";
 
 export default function App() {
+	const [users, setUsers] = useState([]);
 
-  const [users, setUsers] = useState([]);
+	useEffect(() => {
+		fetch("http://localhost:5000/users")
+			.then((res) => res.json())
+			.then((data) => setUsers(data));
+	}, []);
 
-  useEffect(() => {
-    fetch('http://localhost:5000/users')
-      .then(res => res.json())
-      .then(data => setUsers(data))
-  }, [])
-
-  console.log(users)
-  return (
-    <>
-      <Button color="primary">{users[0]?.first_name.toUpperCase()}</Button>
-    </>
-  )
+	console.log(users);
+	return (
+		<>
+			<Button color="primary">{users[0]?.first_name.toUpperCase()}</Button>
+		</>
+	);
 }
