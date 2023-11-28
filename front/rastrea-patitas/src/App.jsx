b552cd6c527d2bbabf89ea64985608dfc0ca87e8
pinoen <pinoen@yahoo.com.ar>
2023-11-28 12:33:39 -0300

    json-server fetch example

--- front/rastrea-patitas/src/App.jsx
+++ front/rastrea-patitas/src/App.jsx
@@ -1,11 +1,20 @@
-import React from "react"
 import { Button } from "@nextui-org/react";
+import React, { useEffect, useState } from "react";
 
 export default function App() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+
+  console.log(users)
   return (
     <>
-      <Button color="primary">Button</Button>
-
+      <Button color="primary">{users[0]?.first_name.toUpperCase()}</Button>
     </>
   )
 }
