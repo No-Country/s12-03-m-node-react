8a4b03dedc8c0a4a14bf8f2e3a5a441d356bebf3
pinoen <pinoen@yahoo.com.ar>
2023-11-29 14:43:05 -0300

    wrap form an a div component and add classes with media queries for bigger screens

--- front/rastrea-patitas/src/features/register/Register.jsx
+++ front/rastrea-patitas/src/features/register/Register.jsx
@@ -13,25 +13,27 @@ const Register = () => {
   }
 
   return (
-    <form className="flex flex-col items-center w-[360px] md:w-[544px] m-10 gap-6 bg-[url('/src/assets/bg-patitas.svg')] bg-cover bg-fondo p-10">
-      <h1 className="text-xl">Registrate</h1>
-      <Input type="email" label="Email" placeholder='Ingresa tu email' variant='underlined' color='secondary' />
-      <Input type={isVisible ? "text" : "password"} label="Contraseña" placeholder="Ingresa tu contraseña" variant='underlined' color='secondary'
-        endContent={
-          <button type='button' onClick={toggleVisibility} className='focus-outline-none'>{isVisible ? <FaRegEyeSlash /> : <FaRegEye />}</button>
-        }
-      />
-      <Input type={isVisible ? "text" : "password"} label="Confirme la contraseña" placeholder="Vuelve a ingresar contraseña" variant='underlined' color='secondary' endContent={<button type='button' onClick={toggleVisibility} className='focus-outline-none'>{isVisible ? <FaRegEyeSlash /> : <FaRegEye />}</button>} />
+    <div className='md:bg-[url("/src/assets/bg-patitas.svg")] md:bg-repeat w-screen h-screen md:flex flex-col justify-center items-center'>
+      <form className="flex flex-col items-center w-[360px] md:w-[544px] m-10 gap-6 bg-[url('/src/assets/bg-patitas.svg')] bg-cover bg-fondo p-10">
+        <h1 className="text-xl">Registrate</h1>
+        <Input type="email" label="Email" placeholder='Ingresa tu email' variant='underlined' color='secondary' />
+        <Input type={isVisible ? "text" : "password"} label="Contraseña" placeholder="Ingresa tu contraseña" variant='underlined' color='secondary'
+          endContent={
+            <button type='button' onClick={toggleVisibility} className='focus-outline-none'>{isVisible ? <FaRegEyeSlash /> : <FaRegEye />}</button>
+          }
+        />
+        <Input type={isVisible ? "text" : "password"} label="Confirme la contraseña" placeholder="Vuelve a ingresar contraseña" variant='underlined' color='secondary' endContent={<button type='button' onClick={toggleVisibility} className='focus-outline-none'>{isVisible ? <FaRegEyeSlash /> : <FaRegEye />}</button>} />
 
-      <Button type="submit" color='primary' variant="ghost" className='w-[255px] mt-10' >Registrarme</Button>
+        <Button type="submit" color='primary' variant="ghost" className='w-[255px] mt-10' >Registrarme</Button>
 
-      <p className='text-sm m-4'>o</p>
+        <p className='text-sm m-4'>o</p>
 
-      <Button startContent={<FcGoogle size={24} />} type="submit" color='secondary' variant="ghost" className='w-[255px] mt-10' >Continuar con Google</Button>
-      <Button startContent={<FaFacebook size={24} />} type="submit" color='primary' variant="solid" className='w-[255px]' >Continuar con Facebook</Button>
+        <Button startContent={<FcGoogle size={24} />} type="submit" color='secondary' variant="ghost" className='w-[255px] mt-10' >Continuar con Google</Button>
+        <Button startContent={<FaFacebook size={24} />} type="submit" color='primary' variant="solid" className='w-[255px]' >Continuar con Facebook</Button>
 
-      <p className='text-xs m-4'>Al registrarse, acepta nuestro Términos de servicio y Política de privacidad . Nunca compartimos su información de contacto con terceros.</p>
-    </form>
+        <p className='text-xs m-4'>Al registrarse, acepta nuestro Términos de servicio y Política de privacidad . Nunca compartimos su información de contacto con terceros.</p>
+      </form>
+    </div>
   )
 }
 
--- front/rastrea-patitas/src/features/register/RegisterData.jsx
+++ front/rastrea-patitas/src/features/register/RegisterData.jsx
@@ -4,27 +4,28 @@ import React from 'react'
 
 const RegisterData = () => {
   return (
-    <form className="flex flex-col items-center w-[360px] md:w-[544px] m-10 gap-2 bg-fondo p-10">
-      <h1 className="text-xl">¡Qué gusto que seas parte!</h1>
-      <h3 className="text-sm pb-8">Completá los campos siguientes con tus datos</h3>
+    <div className='md:bg-[url("/src/assets/bg-patitas.svg")] md:bg-repeat w-screen h-screen md:flex flex-col justify-center items-center'>
+      <form className="flex flex-col items-center w-[360px] md:w-[544px] m-10 gap-2 bg-fondo p-10">
+        <h1 className="text-xl">¡Qué gusto que seas parte!</h1>
+        <h3 className="text-sm pb-8">Completá los campos siguientes con tus datos</h3>
 
-      <label htmlFor='name' className='text-sm pr-56'>Nombre completo:</label>
-      <Input id='name' type="text" placeholder='Nombre y apellido' color='secondary' variant='bordered' />
+        <label htmlFor='name' className='text-sm pr-56'>Nombre completo:</label>
+        <Input id='name' type="text" placeholder='Nombre y apellido' color='secondary' variant='bordered' />
 
-      <label htmlFor='phone' className='text-sm pr-36 pt-5'>Número de telefono <span className='text-gray-500'>(opcional)</span>:</label>
-      <Input id='phone' type="tel" placeholder="Ingresá tu número personal" color='secondary' variant='bordered' />
+        <label htmlFor='phone' className='text-sm pr-36 pt-5'>Número de telefono <span className='text-gray-500'>(opcional)</span>:</label>
+        <Input id='phone' type="tel" placeholder="Ingresá tu número personal" color='secondary' variant='bordered' />
 
-      <h3 className="text-sm pb-8 pt-5 pr-4">Zona para recibir alertas <span className='text-gray-500'>(lo podés editar después)</span></h3>
+        <h3 className="text-sm pb-8 pt-5 pr-4">Zona para recibir alertas <span className='text-gray-500'>(lo podés editar después)</span></h3>
 
-      <div className='flex flex-col items-center justify-center bg-[url("/src/assets/bg-map.jpg")] bg-cover w-[360px] h-[207px]'>
-        <Button type="submit" color='primary' variant="solid" className='w-[255px]' >Añadir ubicación</Button>
-      </div>
+        <div className='flex flex-col items-center justify-center bg-[url("/src/assets/bg-map.jpg")] bg-cover w-[360px] h-[207px]'>
+          <Button type="submit" color='primary' variant="solid" className='w-[255px]' >Añadir ubicación</Button>
+        </div>
 
-      <div className='flex items-center gap-2 pt-8'><input id='accept' type='radio' className='w-6 h-6' /><label htmlFor='accept' >Acepto <span className='underline text-blue-500 cursor-pointer'>términos y condiciones</span></label></div>
+        <div className='flex items-center gap-2 pt-8'><input id='accept' type='radio' className='w-6 h-6' /><label htmlFor='accept' >Acepto <span className='underline text-blue-500 cursor-pointer'>términos y condiciones</span></label></div>
 
-      <Button type="submit" color='primary' variant="bordered" className='w-[255px] mt-10' >Crear cuenta</Button>
-
-    </form>
+        <Button type="submit" color='primary' variant="bordered" className='w-[255px] mt-10' >Crear cuenta</Button>
+      </form>
+    </div>
   )
 }
 
