e673f440000e0b4ac09ca24c8a81766c00b6db31
pinoen <pinoen@yahoo.com.ar>
2023-11-28 23:08:19 -0300

    create register static component

---
+++ front/rastrea-patitas/src/features/register/Register.jsx
@@ -0,0 +1,25 @@
+import { Button, Input } from '@nextui-org/react'
+import React from 'react'
+import googleIcon from '../../assets/icons/google.svg'
+
+const Register = () => {
+  return (
+    <form className="flex flex-col items-center w-[360px] m-10 gap-6 bg-[url('/src/assets/bg-patitas.svg')] bg-cover">
+      <h1 className="text-xl">Registrate</h1>
+      <Input type="email" label="Email" placeholder='Ingresa tu email' variant='underlined' color='secondary' />
+      <Input type="password" label="Contraseña" placeholder="Ingresa tu contraseña" variant='underlined' color='secondary' />
+      <Input type="password" label="Confirme la contraseña" placeholder="Vuelve a ingresar contraseña" variant='underlined' color='secondary' />
+
+      <Button type="submit" color='secondary' variant="ghost" className='w-[255px] mt-10' >Registrarme</Button>
+
+      <p className='text-sm m-4'>o</p>
+
+      <Button type="submit" color='secondary' variant="ghost" className='w-[255px] mt-10' >Continuar con Google</Button>
+      <Button type="submit" color='primary' variant="solid" className='w-[255px]' >Continuar con Facebook</Button>
+
+      <p className='text-xs m-4'>Al registrarse, acepta nuestro Términos de servicio y Política de privacidad . Nunca compartimos su información de contacto con terceros.</p>
+    </form>
+  )
+}
+
+export default Register
