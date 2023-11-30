727b60e7d9bc3653286637647068ea74ff6ace65
pinoen <pinoen@yahoo.com.ar>
2023-11-30 18:16:16 -0300

    create mock components

---
+++ front/rastrea-patitas/src/ui/Layout.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import Header from './Header'
+import { Outlet } from 'react-router-dom'
+import Footer from './Footer'
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
