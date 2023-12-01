dd541a192957a078a89eb957f12476ee102ea999
pinoen <pinoen@yahoo.com.ar>
2023-12-01 11:49:35 -0300

    add logo as an imported variable so to avoid problems on vercel when deploy is done

--- front/rastrea-patitas/src/ui/Header.jsx
+++ front/rastrea-patitas/src/ui/Header.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import logo from '../assets/logos/logo_RastreaPatitas.svg'
 
 const Header = () => {
   return (
     <div className='flex justify-between items-center bg-orange-200 h-40 p-10'>
       <div className='flex justify-center items-center'>
-        <img src="/src/assets/logos/logo_RastreaPatitas.svg" alt="Logo" />
+        <img src={logo} alt="Logo" />
       </div>
       <p className='text-xl font-bold'>USERNAME</p>
     </div>
