8ed20922da937637b8768ff63cebece363b538fa
Juan Nebbia <juan.nebbia@gmail.com>
2023-12-09 19:31:40 -0300

    feat(backend/users): include profile_img on user creation

--- backend/src/controllers/users.controller.js
+++ backend/src/controllers/users.controller.js
@@ -1,6 +1,7 @@
 import Pets from "../models/Pets.js";
 import Users from "../models/Users.js";
 import { HttpCodes } from "../utils/HTTPCodes.util.js";
+import { handleImageDelete, handleImageUpload } from "../utils/imageHandle.js";
 
 export const getUsers = async (req, res, next) => {
     try {
@@ -37,8 +38,12 @@ export const updateUserById = async (req, res, next) => {
     try {
         const { id } = req.params
         const userPayload = req.body
-        if(Object.keys(userPayload).length === 0) {
+        if(Object.keys(userPayload).length === 0 && req.files.length === 0) {
             return res.status(HttpCodes.CODE_BAD_REQUEST).json({ message: "empty payload" });
+        }
+        if (req.files) {
+            const uploadedImage = await handleImageUpload(req.files.profile_img);
+            userPayload.profile_img = uploadedImage[0]
         }
         const user = await Users.findById(id);
         if (!user) {
@@ -48,6 +53,25 @@ export const updateUserById = async (req, res, next) => {
         return res.status(HttpCodes.CODE_SUCCESS).json(updatedUser);
     } catch (error) {
         res.status(HttpCodes.CODE_INTERNAL_SERVER_ERROR).send(error)
+    }
+}
+
+export const deleteImageFromUserById = async (req, res) => {
+    try {
+        const { id, image_id } = req.params;
+        const user = await Users.findById(id);
+
+        if (!user) return res.status(HttpCodes.CODE_NOT_FOUND).json({ message: "El usuario no ha sido encontrado" });
+
+        if (Object.keys(user.profile_img).length > 2) return res.status(HttpCodes.CODE_NOT_FOUND).json({ message: "La imagen no ha sido encontrada" });
+        
+        await handleImageDelete(image_id);
+
+        await Users.findByIdAndUpdate(id, { profile_img: {} });
+
+        return res.status(HttpCodes.CODE_SUCCESS).json({ message: "La imagen ha sido eliminada" });
+    } catch (error) {
+        return res.status(HttpCodes.CODE_INTERNAL_SERVER_ERROR).json({ message: error.message });
     }
 }
 
