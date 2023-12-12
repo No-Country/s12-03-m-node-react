8edb94a94cc78483919ffa38a98e55a68fe31d11
Dalian Aguirre <danathap@gmail.com>
2023-12-12 08:59:11 -0500

    home update

--- front/rastrea-patitas/src/features/home/Home.jsx
+++ front/rastrea-patitas/src/features/home/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
 
     return (
         <>        
-            <div className='bg-[url("/src/assets/bg-patitas.svg")] h-screen flex flex-col items-center'>
+            <div className='bg-[url("/src/assets/bg-patitas.svg")] flex flex-col items-center'>
                 <div className="flex flex-col items-center justify-center mb-8 w-[360px] md:w-[600px] lg:w-[900px] gap-6 mt-8 mx-2">
 
                     <div className="flex items-center justify-center space-x-2">
