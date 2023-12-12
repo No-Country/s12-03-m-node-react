4a8a900b8b1f54a60ed2dde4babb3fff166ecf84
Agustin Gonzalez <agustin_.gonzalez_@hotmail.com>
2023-12-12 20:14:47 -0300

    local host 5173

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,40 +1,42 @@
-import cookieParser from 'cookie-parser';
-import express from 'express';
-import bodyParser from 'body-parser';
+import cookieParser from "cookie-parser";
+import express from "express";
+import bodyParser from "body-parser";
 import swaggerUi from "swagger-ui-express";
-import {specs} from './doc/swaggerConfig.js';
-import routes from './routes/index.js';
-import fileUpload from 'express-fileupload';
-import passport from 'passport';
-import initializePassport from './middlewares/passport.middleware.js';
-import session from 'express-session';
-import { SECRET_KEY, SESSION_KEY, CSS_URL } from './config/envConfig.js';
-import cors from 'cors'
+import { specs } from "./doc/swaggerConfig.js";
+import routes from "./routes/index.js";
+import fileUpload from "express-fileupload";
+import passport from "passport";
+import initializePassport from "./middlewares/passport.middleware.js";
+import session from "express-session";
+import { SECRET_KEY, SESSION_KEY, CSS_URL } from "./config/envConfig.js";
+import cors from "cors";
 
 const app = express();
 
 //cors
-app.use(cors({
-  origin: ['https://rastreapatitas.vercel.app/', 'http://localhost:3000', "https://s12-03-m-node-react.vercel.app/api-docs/"],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-}));
-
+app.use(
+	cors({
+		origin: ["https://rastreapatitas.vercel.app/", "http://localhost:5173", "https://s12-03-m-node-react.vercel.app/api-docs/"],
+		methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+		credentials: true,
+	})
+);
 
 app.use(express.json());
-app.use(fileUpload({
-    useTempFiles: true,
-    tempFileDir: './uploads',
-
-}));
+app.use(
+	fileUpload({
+		useTempFiles: true,
+		tempFileDir: "./uploads",
+	})
+);
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 initializePassport();
 app.use(passport.initialize());
 app.use(
-  session({
-    name: SESSION_KEY,
+	session({
+		name: SESSION_KEY,
 		secret: SECRET_KEY,
 		resave: false,
 		saveUninitialized: false,
@@ -42,16 +44,8 @@ app.use(
 );
 app.use(passport.session());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs, { customCssUrl: CSS_URL }));
 
-app.use(
-    "/api-docs",
-    swaggerUi.serve,
-    swaggerUi.setup(specs, { customCssUrl: CSS_URL })
-  );
+app.use("/api", routes);
 
-app.use("/api", routes)
-
-
-export default app
-
-
+export default app;
