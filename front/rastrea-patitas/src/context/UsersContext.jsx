d59b6fa8f887c9b7d1f1dad91a21860a088501bd
Agustin Gonzalez <agustin_.gonzalez_@hotmail.com>
2023-12-15 16:05:11 -0300

    login

--- front/rastrea-patitas/src/context/UsersContext.jsx
+++ front/rastrea-patitas/src/context/UsersContext.jsx
@@ -1,25 +1,25 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import React from "react";
-import { createContext, useEffect, useState } from "react";
-import { getUsers } from "../services/apiPatitas";
+import { createContext, useState } from "react";
+import { getUsers, login } from "../services/apiPatitas";
 
 const UsersContext = createContext();
 
 const UsersProvider = ({ children }) => {
-	const [users, setUsers] = useState([]);
+	const [user, setUser] = useState([]);
 
-	useEffect(() => {
-		getUsers()
-			.then((data) => {
-				setUsers(data);
-			})
-			.catch((error) => {
-				console.log(error);
-			});
-	}, []);
+	const loginUser = async (user) => {
+		try {
+			const response = await login(user);
 
-	return <UsersContext.Provider value={{ users, setUsers }}>{children}</UsersContext.Provider>;
+			return response;
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	return <UsersContext.Provider value={{ loginUser, user, setUser }}>{children}</UsersContext.Provider>;
 };
 
 export { UsersContext, UsersProvider };
--- front/rastrea-patitas/src/services/apiPatitas.js
+++ front/rastrea-patitas/src/services/apiPatitas.js
@@ -1,67 +1,68 @@
 import axios from "axios";
 
-const URL = 'https://s12-03-m-node-react.vercel.app/api';
+const URL = "https://s12-03-m-node-react.vercel.app/api";
 
 const getUsers = async () => {
-  try {
-    const result = await axios.get(`${URL}/users`)
-    return result.data
-  } catch (error) {
-    console.log(error)
-  }
-}
+	try {
+		const result = await axios.get(`${URL}/users`);
+		return result.data;
+	} catch (error) {
+		console.log(error);
+	}
+};
 
 const getPets = async () => {
-  try {
-    const result = await axios.get(`${URL}/pets`)
-    return result.data
-  } catch (error) {
-    console.log(error)
-  }
-}
+	try {
+		const result = await axios.get(`${URL}/pets`);
+		return result.data;
+	} catch (error) {
+		console.log(error);
+	}
+};
 
 const loginWithGoogle = async () => {
-  try {
-    const result = await axios.get(`${URL}/session/auth/google`)
-    return result.data
-  } catch (error) {
-    console.log(error)
-  }
-}
+	try {
+		const result = await axios.get(`${URL}/session/auth/google`);
+		return result.data;
+	} catch (error) {
+		console.log(error);
+	}
+};
 
 const loginWithFacebook = async () => {
-  try {
-    const result = await axios.get(`${URL}/session/auth/facebook`)
-    return result.data
-  } catch (error) {
-    console.log(error)
-  }
-}
+	try {
+		const result = await axios.get(`${URL}/session/auth/facebook`);
+		return result.data;
+	} catch (error) {
+		console.log(error);
+	}
+};
 
 const login = async (data) => {
-  try {
-    const result = await axios.post(`${URL}/session/login`, data, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-    return result.data
-  } catch (error) {
-    console.log(error)
-  }
-}
+	try {
+		const result = await axios.post(`${URL}/session/login`, data, {
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+
+		return result.data;
+	} catch (error) {
+		return error.response;
+	}
+};
 
 const registerUser = async (data) => {
-  try {
-    const result = await axios.post(`${URL}/session/register`, data, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-    return result.data
-  } catch (error) {
-    console.log(error)
-  }
-}
+	try {
+		const result = await axios.post(`${URL}/session/register`, data, {
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+		return result.data;
+	} catch (error) {
+		console.log(error);
+	}
+};
 
-export { getUsers, getPets, loginWithGoogle, loginWithFacebook, login, registerUser }
+export { getUsers, getPets, loginWithGoogle, loginWithFacebook, login, registerUser };
