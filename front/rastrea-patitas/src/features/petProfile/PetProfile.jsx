17c4e2b810c0a2568a0bf4bc15f0fda32fbb1b56
hernan-varela <hernanvarela45@gmail.comm>
2023-12-15 10:38:33 -0300

    version desktop perfil mascota

--- front/rastrea-patitas/src/features/petProfile/PetProfile.jsx
+++ front/rastrea-patitas/src/features/petProfile/PetProfile.jsx
@@ -25,73 +25,129 @@ function PetProfile() {
   }
 
   return (
-    <div className='bg-[url("src/assets/bg-patitas.svg")] bg-repeat max-w-full'>
-      <NavProfile name={name} />
-      <PetCard state={state} img={img} editProfile={editProfile} />
+    <>
+      {/* seccion para pantallas pequeñas */}
+      <div className='bg-[url("src/assets/bg-patitas.svg")] bg-repeat max-w-full md:hidden'>
+        <NavProfile name={name} />
+        <PetCard state={state} img={img} editProfile={editProfile} />
 
-      {!editProfile ? (
-        <div className=" flex justify-around my-4">
-          <Button
-            color="primary"
-            variant="bordered"
-            className="border-moradoMain text-letra font-medium w-36 "
-          >
-            Eliminar perfil
-          </Button>
-          <Button
-            color="primary"
-            variant="bordered"
-            className="border-moradoMain text-letra font-medium w-36 "
-            onClick={handlerEditProfile}
-          >
-            Editar perfil
-          </Button>
-        </div>
-      ) : (
-        <div className=" flex justify-around my-4">
-          <Button
-            color="primary"
-            variant="bordered"
-            className="border-moradoMain text-letra font-medium w-36 "
-            onClick={handlerCancelEditProf}
-          >
-            Cancelar
-          </Button>
-          <Button
-            color="primary"
-            variant="bordered"
-            className="border-moradoMain text-letra font-medium w-36 "
-          >
-            Guardar perfil
-          </Button>
-        </div>
-      )}
+        {!editProfile ? (
+          <div className=" flex justify-around my-4">
+            <Button
+              color="primary"
+              variant="bordered"
+              className="border-moradoMain text-letra font-medium w-36 "
+            >
+              Eliminar perfil
+            </Button>
+            <Button
+              color="primary"
+              variant="bordered"
+              className="border-moradoMain text-letra font-medium w-36 "
+              onClick={handlerEditProfile}
+            >
+              Editar perfil
+            </Button>
+          </div>
+        ) : (
+          <div className=" flex justify-around my-4">
+            <Button
+              color="primary"
+              variant="bordered"
+              className="border-moradoMain text-letra font-medium w-36 "
+              onClick={handlerCancelEditProf}
+            >
+              Cancelar
+            </Button>
+            <Button
+              color="primary"
+              variant="bordered"
+              className="border-moradoMain text-letra font-medium w-36 "
+            >
+              Guardar perfil
+            </Button>
+          </div>
+        )}
 
-      <div className="mx-5">
-        <p className=" text-lg font-bold  text-letra">{name}</p>
-        <p className=" text-sm font-normal text-[#6B7A85]">
-          Registrado el {registeredAgo}
-        </p>
+        <div className="mx-5">
+          <p className=" text-lg font-bold  text-letra">{name}</p>
+          <p className=" text-sm font-normal text-[#6B7A85]">
+            Registrado el {registeredAgo}
+          </p>
+        </div>
+
+        <InfoLocation ubicacion={"Buenos Aires"} />
+
+        <PetState editProfile={editProfile} />
+
+        {editProfile ? (
+          <EditPetCharacteristics />
+        ) : (
+          <PetCharacteristics
+            type={"gato"}
+            sex={"macho"}
+            eyes={"Ojos claros"}
+            hair={"Pelo corto"}
+            color={"Bicolor"}
+          />
+        )}
+
+        <QrCode />
       </div>
 
-      <InfoLocation ubicacion={"Buenos Aires"} />
+      {/* seccion para pantallas medianas y grandes */}
+      <div className=" hidden md:block ">
+        <div className="w-2/3 ">
+          <div className="flex flex-row">
+            <div className="mx-5">
+              <p className=" text-4xl font-bold  text-letra">{name}</p>
+              <p className=" text-xl font-bold text-[#6B7A85]">
+                Registrado el {registeredAgo}
+              </p>
+            </div>
+            {!editProfile ? (
+              <div className=" flex justify-between w-1/2 ">
+                <Button
+                  color="primary"
+                  variant="bordered"
+                  className="border-moradoMain text-letra font-medium w-36 "
+                >
+                  Eliminar perfil
+                </Button>
+                <Button
+                  color="primary"
+                  variant="bordered"
+                  className="border-moradoMain text-letra font-medium w-36 "
+                  onClick={handlerEditProfile}
+                >
+                  Editar perfil
+                </Button>
+              </div>
+            ) : (
+              <div className=" flex justify-around ">
+                <Button
+                  color="primary"
+                  variant="bordered"
+                  className="border-moradoMain text-letra font-medium w-36 "
+                  onClick={handlerCancelEditProf}
+                >
+                  Cancelar
+                </Button>
+                <Button
+                  color="primary"
+                  variant="bordered"
+                  className="border-moradoMain text-letra font-medium w-36 "
+                >
+                  Guardar perfil
+                </Button>
+              </div>
+            )}
+          </div>
 
-      <PetState editProfile={editProfile} />
-
-      {editProfile ? (
-        <EditPetCharacteristics />
-      ) : (
-        <PetCharacteristics
-          type={"gato"}
-          sex={"macho"}
-          eyes={"Ojos claros"}
-          hair={"Pelo corto"}
-          color={"Bicolor"}
-        />
-      )}
-
-      <QrCode />
-    </div>
+          <PetCard state={state} img={img} editProfile={editProfile} />
+        </div>
+      </div>
+    </>
   );
 }
 
