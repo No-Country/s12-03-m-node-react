aa4c08695d46d55b147766ec5551572331afe23a
pinoen <pinoen@yahoo.com.ar>
2023-12-17 18:12:35 -0300

    wrap app with alerts provider

--- front/rastrea-patitas/src/main.jsx
+++ front/rastrea-patitas/src/main.jsx
@@ -5,15 +5,18 @@ import './index.css'
 import { NextUIProvider } from '@nextui-org/react'
 import { PetsProvider } from './context/PetsContext.jsx'
 import { UsersProvider } from './context/UsersContext.jsx'
+import { AlertsProvider } from './context/AlertsContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NextUIProvider>
-      <UsersProvider>
-        <PetsProvider>
-          <App />
-        </PetsProvider>
-      </UsersProvider>
+      <AlertsProvider>
+        <UsersProvider>
+          <PetsProvider>
+            <App />
+          </PetsProvider>
+        </UsersProvider>
+      </AlertsProvider>
     </NextUIProvider>
   </React.StrictMode>,
 )
