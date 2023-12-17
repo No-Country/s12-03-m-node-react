73bcecaeee3a35546b072c46c3840d328abd57c7
pinoen <pinoen@yahoo.com.ar>
2023-12-17 19:15:58 -0300

    fix grammar error

--- front/rastrea-patitas/src/features/newAdvertisement/PublicationMade.jsx
+++ front/rastrea-patitas/src/features/newAdvertisement/PublicationMade.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import PublicationPet from "../../assets/PublicationPet.svg"
+import PublicationPet from "../../assets/publicationPet.svg"
 function PublicationMade() {
   return (
     <div className='bg-[url("/src/assets/bg-patitas.svg")] bg-repeat flex flex-col  items-center h-screen		'>
       <h1 className="text-3xl	font-semibold	pt-10	text-center ">¡Publicación realizada!</h1>
       <h2 className="pt-9	text-center ">Aguarda unos segundos mientras generamos tu cartel informativo...</h2>
       <div className="h-screen	flex items-end">
-      <img src={PublicationPet} alt="" className=""/></div>
+        <img src={PublicationPet} alt="" className="" /></div>
     </div>
   );
 }
