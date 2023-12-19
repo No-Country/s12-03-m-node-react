2f431d7eb20f93c5ff402896c9959be6c60c00dc
pinoen <pinoen@yahoo.com.ar>
2023-12-19 15:13:05 -0300

    add google map functionality

--- front/rastrea-patitas/src/context/AlertsContext.jsx
+++ front/rastrea-patitas/src/context/AlertsContext.jsx
@@ -10,6 +10,31 @@ const AlertsContext = createContext();
 const AlertsProvider = ({ children }) => {
   const [alerts, setAlerts] = useState([]);
   const [alert, setAlert] = useState(null);
+  const [position, setPosition] = useState(null);
+  const [error, setError] = useState(null);
+  const [openMap, setOpenMap] = useState(false);
+
+  const handleOpenMap = () => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const latitud = position.coords.latitude;
+          const longitud = position.coords.longitude;
+          console.log("Latitud:", latitud);
+          console.log("Longitud:", longitud);
+
+          setPosition({ lat: latitud, lng: longitud });
+          setOpenMap(true);
+        },
+        (error) => {
+          console.error("Error al obtener la ubicación:", error.message);
+          setError(error.message);
+        }
+      );
+    } else {
+      setError("La geolocalización no está disponible en este dispositivo.");
+    }
+  };
 
   useEffect(() => {
     getAlerts().then((alerts) => {
@@ -18,12 +43,9 @@ const AlertsProvider = ({ children }) => {
       console.log(error);
     })
   }, [])
-  // const getUserAlertsByUserId = (userId) => {
-  //   return alerts.filter((alert) => alert.userId === userId);
-  // }
 
   return (
-    <AlertsContext.Provider value={{ alerts, setAlerts, alert, setAlert }}>
+    <AlertsContext.Provider value={{ alerts, setAlerts, alert, setAlert, position, setPosition, error, setError, openMap, setOpenMap, handleOpenMap }}>
       {children}
     </AlertsContext.Provider>
   )
