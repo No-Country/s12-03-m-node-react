3ee7d480ee77ec49506ea36e32d6b2277fa5768a
pinoen <pinoen@yahoo.com.ar>
2023-12-18 21:14:07 -0300

    add basic validations

--- front/rastrea-patitas/src/features/register/Register.jsx
+++ front/rastrea-patitas/src/features/register/Register.jsx
@@ -11,8 +11,7 @@ import { registerUser } from '../../services/api';
 
 const Register = () => {
   const [isVisible, setIsVisible] = useState(false)
-
-  const { handleSubmit, register, formState: { errors } } = useForm()
+  const { handleSubmit, register, formState: { errors }, getValues } = useForm()
 
   const toggleVisibility = () => {
     setIsVisible(!isVisible)
@@ -20,27 +19,44 @@ const Register = () => {
 
   const onSubmit = handleSubmit(async (data) => {
     console.log(data)
-    try {
-      const response = await registerUser(data)
-      console.log(response)
-    } catch (error) {
-      console.log(error)
-    }
+    // try {
+    //   const response = await registerUser(data)
+    //   console.log(response)
+    // } catch (error) {
+    //   console.log(error)
+    // }
   })
 
   return (
     <div className='md:bg-[url("/src/assets/bg-patitas.svg")] md:bg-repeat w-screen h-screen md:flex flex-col justify-center items-center'>
       <form onSubmit={onSubmit} className="flex flex-col items-center w-[360px] md:w-[544px] m-1 gap-6 bg-[url('/src/assets/bg-patitas.svg')] bg-cover bg-fondo p-10">
         <h1 className="text-xl">Registrate</h1>
-        <Input type="email" label="Email" placeholder='Ingresa tu email' variant='underlined' color='secondary' {...register("email", { required: true })} />
+        <Input type="email" label="Email" placeholder='Ingresa tu email' variant='underlined' color='secondary'
+          {...register("email", {
+            required: "El campo email es requerido",
+            pattern: {
+              value: /^[A-Za-z][A-Za-z0-9._%+-]*@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
+              message: "Ingresa un email valido",
+            },
+          })}
+        />
+
+        <p className="text-red-500 text-sm">{errors.email?.message}</p>
+
         <Input type={isVisible ? "text" : "password"} label="Contraseña" placeholder="Ingresa tu contraseña" variant='underlined' color='secondary'
           endContent={
             <button type='button' onClick={toggleVisibility} className='focus-outline-none'>{isVisible ? <FaRegEyeSlash /> : <FaRegEye />}</button>
           }
-          {...register("password", { required: true })}
+          {...register("password", { required: "El campo contraseña es requerido" })}
         />
+
+        <p className="text-red-500 text-sm">{errors.password?.message}</p>
+
         <Input type={isVisible ? "text" : "password"} label="Confirme la contraseña" placeholder="Vuelve a ingresar contraseña" variant='underlined' color='secondary' endContent={<button type='button' onClick={toggleVisibility} className='focus-outline-none'>{isVisible ? <FaRegEyeSlash /> : <FaRegEye />}</button>}
+          {...register("confirmPassword", { required: "El campo confirmar contraseña es requerido", validate: (value) => value === getValues("password") || "Las contraseña no coinciden" })}
         />
+
+        <p className="text-red-500 text-sm">{errors.confirmPassword?.message}</p>
 
         <Button type="submit" color='primary' variant="ghost" className='w-[255px] mt-10'>Registrarme</Button>
 
